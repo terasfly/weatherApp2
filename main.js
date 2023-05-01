d94d838fc86de0a808b846ee0ee69b44ea5aaca3
terasfly <84103159+terasfly@users.noreply.github.com>
2023-05-01 23:45:36 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,15 +2,30 @@ const city = document.getElementById('city');
 const api = '497bea2e477443d8bb185600232504';
 const tempNow = document.getElementById('temp__now-degrs')
 let nowImage = document.getElementById('weahter__condition-nowImage')
+const liveTime = document.getElementById('time__date')
 
+const todayDate = document.getElementById('today__date')
+
+const tempDegres = document.querySelector('.temp__degrs')
 
 const btn = document.getElementById('btn')
 
 
 
 
+const firstBlockImage = document.querySelector('.firstBlockImage')
+
+
+
+
+
+
 
 window.onload = () => {
+
+
+
+
     navigator.geolocation.getCurrentPosition((position) => {
         const lat = position.coords.latitude;
         const lon = position.coords.longitude;
@@ -20,6 +35,8 @@ window.onload = () => {
         fetch(url)
             .then((Response) => Response.json())
             .then((data) => {
+                updateHour(0, document.querySelector('.time__hourly'), firstBlockImage, tempDegres, data);
+
 
                 // load page get location
                 const cityName = data.location.name;
@@ -27,17 +44,14 @@ window.onload = () => {
                 // get current temp from location
                 const dataTempNow = data.current.temp_c
                 tempNow.textContent = dataTempNow;
-                // get current weather condition from location
+                // show real date 
+                today.textContent
 
-                // const dataCondNow = data.current.condition.icon
-                //     // replace link to link which need for me
-                // const dataCondNowReplace = dataCondNow.replace('//cdn.weatherapi.com/weather', 'images')
-                // console.log(dataCondNowReplace)
-                //     // too current location add new image
-                // nowImage.setAttribute('src', dataCondNowReplace);
+
 
                 getIconOfWeather(data)
-
+                today(data)
+                time(data)
 
 
 
@@ -50,20 +64,24 @@ window.onload = () => {
 btn.addEventListener('click', () => {
     const searchCity = document.getElementById('search__city').value
 
-    console.log(searchCity)
-        // getIconOfWeather()
+
+
+
+    // getIconOfWeather()
 
     const url2 = `https://api.weatherapi.com/v1/forecast.json?key=${api}&q=${searchCity}`;
     fetch(url2)
         .then((Response) => Response.json())
         .then((data) => {
+
             console.log(url2)
             cityName = data.location.name;
             city.textContent = cityName;
             dataTempNow = data.current.temp_c
             tempNow.textContent = dataTempNow;
             getIconOfWeather(data)
-
+            today(data)
+            time(data)
 
 
         })
@@ -73,4 +91,37 @@ function getIconOfWeather(data) {
     const getIcon = data.current.condition.icon
     const replace = getIcon.replace('//cdn.weatherapi.com/weather', 'images')
     nowImage.setAttribute('src', replace);
+}
+
+function today(data) {
+    const getDate = data.location.localtime
+    const justDate = getDate.split(' ')[0]
+    todayDate.textContent = justDate
+    console.log(justDate)
+
+
+
+}
+
+
+
+function updateHour(hourIndex, timeElement, imageElement, tempElement, data) {
+    const hour = data.forecast.forecastday[0].hour[hourIndex];
+    const justTime = hour.time.split(' ')[1];
+    timeElement.textContent = justTime;
+
+    const image = hour.condition.icon.replace('//cdn.weatherapi.com/weather', 'images');
+    imageElement.setAttribute('src', image);
+
+    const temp = hour.temp_c;
+    console.log(temp);
+    tempElement.textContent = temp;
+}
+
+function time(data) {
+    const getLiveTime = data.location.localtime
+    console.log(getLiveTime)
+    const localTime = getLiveTime.split(' ')[1]
+    console.log(localTime)
+    liveTime.textContent = localTime
 }
